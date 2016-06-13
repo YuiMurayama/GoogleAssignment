a2df533f37c22f0303b46a07655ab2e017813cd9
YuiMurayama <yui.murayama913@gmail.com>
2016-06-13 19:12:16 +0900

    added

--- src/Matrix.java
+++ src/Matrix.java
@@ -5,15 +5,14 @@ import java.io.PrintWriter;
 
 class Matrix {
 	public static void main(String args[]) {
-
 		try {
-			FileWriter fw = new FileWriter("f:\\test.csv", false);
+			FileWriter fw = new FileWriter("test3.csv", false);
 			PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
-			for (int n = 1; n < 10; n++) {
+			for (int n = 450; n < 451; n++) {
+				int countNum = 10;
 				double[][] a = new double[n][n]; // Matrix A
 				double[][] b = new double[n][n]; // Matrix B
 				double[][] c = new double[n][n]; // Matrix C
-
 				// Initialize the matrices to some values.
 				int i, j;
 				for (i = 0; i < n; i++) {
@@ -23,17 +22,21 @@ class Matrix {
 						c[i][j] = 0;
 					}
 				}
-				long begin = System.currentTimeMillis();
-				for (i = 0; i < n; i++) {
-					for (j = 0; j < n; j++) {
-						for (int k = 0; k < n; k++) {
-							c[i][j] += a[i][k] * b[k][j];
+				for (int count = 0; count < countNum; count++) {
+					long begin = System.currentTimeMillis();
+
+					for (i = 0; i < n; i++) {
+						for (j = 0; j < n; j++) {
+							for (int k = 0; k < n; k++) {
+								c[i][j] += a[i][k] * b[k][j];
+							}
 						}
+
 					}
+					long end = System.currentTimeMillis();
+					double time = (end - begin) / 1000.0 ;
+					System.out.printf("time: %.6f sec\n", time);
 				}
-				long end = System.currentTimeMillis();
-				double time = (end - begin) / 1000.0;
-				System.out.printf("time: %.6f sec\n", time);
 
 				// Print C for debugging. Comment out the print before measuring
 				// the
@@ -51,13 +54,17 @@ class Matrix {
 				System.out.println(n);
 				pw.print(n);
 				pw.print(",");
-				pw.print(time);
+//				pw.print(time);
+				// pw.print(",");
+				// pw.print((n^3)/1000000);
 				pw.println();
 			}
+
 			pw.close();
 
 		} catch (IOException e) {
 			// TODO 自動生成された catch ブロック
+
 			e.printStackTrace();
 		} // ※１
 	}
