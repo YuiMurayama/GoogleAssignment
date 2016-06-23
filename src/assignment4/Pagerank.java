a00a5ee8ecfc73b63a5eebcc23c91c8946400833
YuiMurayama <yui.murayama913@gmail.com>
2016-06-23 10:45:19 +0900

    reviesed

--- src/assignment4/Pagerank.java
+++ src/assignment4/Pagerank.java
@@ -1,5 +1,4 @@
 package assignment4;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
@@ -11,37 +10,32 @@ class Pagerank {
 	public static void main(String args[]) {
 		try {
 			File file = new File(
-					"/Users/murayamayui/Documents/eclipse/GoogleAssignment/src/assignment4/large_data.txt");
+					"/Users/murayamayui/Documents/eclipse/GoogleAssignment/src/assignment4/medium_data.txt");
 			ArrayList<Node> nodeArray = new ArrayList<Node>();
-			
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			String str =br.readLine();
 
-			int num =Integer.parseInt(str);
-			
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String str = br.readLine();
+			int nodeNum = Integer.parseInt(str);
 			// ノードの生成
-			for (int index = 0; index <  num; index++) {
+			for (int index = 0; index < nodeNum; index++) {
 				str = br.readLine();
 				Node node = new Node();
 				node.nodeName = str;
 				nodeArray.add(node);
 			}
-//			printNode(nodeArray);
-			
-			
 			str = br.readLine();
 			// パスの生成
-//			BufferedReader br = new BufferedReader(new FileReader(file));
-//			String str;
 			while ((str = br.readLine()) != null) {
 				nodeArray = makePass(str, nodeArray);
 			}
 			br.close();
-			// pageRankの計算
-			for (int i = 0; i < 50; i++) {
-				nodeArray = calPageRank(nodeArray); // ページランク計算
+
+			// pageRankの計算をi回
+			for (int i = 0; i < 150; i++) {
+				nodeArray = calPageRank(nodeArray);
 			}
-//			printNode(nodeArray);
+			//結果を出力
+			printNode(nodeArray);
 		} catch (FileNotFoundException e) {
 			System.out.println(e);
 		} catch (IOException e) {
@@ -67,9 +61,8 @@ class Pagerank {
 		double allPoint = 0;
 		for (int i = 0; i < nodeArray.size(); i++) {
 			System.out.print(nodeArray.get(i).nodeName);
-			System.out.print(" 得点は" +  nodeArray.get(i).point);
-			System.out.print(" 次の得点は" + nodeArray.get(i).nextPoint);
-
+			System.out.print("の得点は" + nodeArray.get(i).point);
+//			System.out.print(" 次の得点は" + (int) nodeArray.get(i).nextPoint);
 			allPoint += nodeArray.get(i).point;
 			System.out.println();
 		}
@@ -77,16 +70,19 @@ class Pagerank {
 		// System.out.println();
 	}
 
-	// 次の得点を計算する
+	// 次の得点を計算してnextPointにいれたのち、pointと交換する
 	static ArrayList<Node> calPageRank(ArrayList<Node> nodeArray) {
 		for (int i = 0; i < nodeArray.size(); i++) {
+			if (nodeArray.get(i).linkList.size() == 0) {
+				nodeArray.get(i).nextPoint += nodeArray.get(i).point;
+			}
 			double addPoint = nodeArray.get(i).point
 					/ nodeArray.get(i).linkList.size();
-			for (int s = 0; s < nodeArray.size(); s++) {
-				for (int t = 0; t < nodeArray.get(i).linkList.size(); t++) {
-					if (nodeArray.get(s).nodeName.equals(nodeArray.get(i).linkList
-							.get(t))) {
-						nodeArray.get(s).nextPoint += addPoint;
+			for (int t = 0; t < nodeArray.get(i).linkList.size(); t++) {
+				for (int a = 0; a < nodeArray.size(); a++) {
+					if (nodeArray.get(i).linkList.get(t).equals(
+							nodeArray.get(a).nodeName)) {
+						nodeArray.get(a).nextPoint += addPoint;
 					}
 				}
 			}
@@ -99,11 +95,10 @@ class Pagerank {
 	static ArrayList<Node> exchangePoint(ArrayList<Node> nodeArray) {
 		double error = 0;
 		for (int i = 0; i < nodeArray.size(); i++) {
-			// double tempPoin = nodeArray.get(i).point;
 			error += nodeArray.get(i).Exchange();
 		}
 		error = Math.sqrt(error / nodeArray.size());
-		System.out.println(error);
+//		System.out.println(error);
 		return nodeArray;
 	}
 }
